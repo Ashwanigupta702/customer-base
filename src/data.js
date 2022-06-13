139cfb6add9612ac962c515c1db78062aecf878b
Ashwanigupta702 <107405233+Ashwanigupta702@users.noreply.github.com>
2022-06-13 18:45:47 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,30 @@
+import img1 from './Img/img1.jpeg';
+import img2 from './Img/img2.jpeg';
+import img3 from './Img/img3.jpeg';
+
+const data = {
+    productData:[
+        {
+            id:1,
+            img:img1,
+            title:'Rice',
+            desc:'',
+            price: 45
+        },
+        {
+            id:2,
+            img:img2,
+            title:'Water Melon',
+            desc:'',
+            price: 30
+        },
+        {
+            id:3,
+            img:img3,
+            title:'Oranges',
+            desc:'',
+            price: 40
+        },
+    ],
+};
+export default data;
